edd34ccacdb6b5d6157524b48d2d6253710866aa
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2024-06-25 21:04:17 +0500

    create pages & routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Projects from './pages/Projects';
+Home
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='text-3xl text-blue-950'>text</p>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/signin' element={<Signin/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/projects' element={<Projects/>} />
+
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
