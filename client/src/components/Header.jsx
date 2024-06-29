85a3339bd7e88a83d5ae3eac4f94db489f799037
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2024-06-29 22:52:05 +0500

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -36,7 +36,7 @@ function Header() {
         </Button>
 
         <Link to={'/signin'}>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='purpleToBlue' outline>
             Sign In
           </Button>
         </Link>
