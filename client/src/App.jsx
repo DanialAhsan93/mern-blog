8eb6e17c830d90988ba3444445968bb69baa737b
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2024-09-08 14:35:19 +0500

    create and add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
@@ -6,22 +6,23 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Projects from './pages/Projects';
 import Header from './components/Header';
+import FooterComp from './components/Footer';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-       <Header/>
+        <Header />
         <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-        <Route path='/signin' element={<Signin/>} />
-        <Route path='/signup' element={<Signup/>} />
-        <Route path='/projects' element={<Projects/>} />
-
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/projects' element={<Projects />} />
         </Routes>
+        <FooterComp />
       </BrowserRouter>
     </>
   )
