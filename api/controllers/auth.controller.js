58f374dbc1c3ce8e95dbfba9cf809fd47fbe6dd7
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2024-09-13 22:46:18 +0500

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -25,4 +26,46 @@ export const signup = async (req, res, next) => {
   }
 
 
-}
+};
+
+export const signin =async (req, res, next) => {
+  const {email, password} = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400,'All fileds are required'))
+  };
+
+  try {
+    const validUser =await User.findOne({email});
+
+    if (!validUser) {
+      return next(errorHandler(404,'User not found'));
+    };
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+    if (!validPassword) {
+      return next(errorHandler(400,'Invalid password'))
+    };
+
+    const token = jwt.sign(
+      {id: validUser._id},
+      process.env.JWT_Secret
+    );
+
+    // this is destructuring technique to hide the password;
+
+    const {password : pass, ...rest} = validUser._doc ;
+
+    // this is another way to hide the password 
+
+    // const rest = Object.assign({}, validUser._doc);
+    // delete rest.password;
+
+    res.status(200).cookie('access_token',token,{
+      httpOnly : true
+    }).json(rest);
+
+  } catch (error) {
+    next(error)
+  }
+};
