fb643440607686d326e9ad29414c8d5ddc09ffd5
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2024-09-20 15:38:50 +0500

    add update user api route

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from './error.js';
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    return next(errorHandler(401, 'Unathorized'));
+  };
+
+  jwt.verify(
+    token,
+    process.env.JWT_Secret,
+    (err, user) => {
+      if (err) {
+        next(errorHandler(401, 'Unathorized'))
+      };
+
+      req.user = user;
+      next();
+    }
+  );
+}
+
