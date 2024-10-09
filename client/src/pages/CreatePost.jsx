d024f3b715731408c611980e9bf2bb480074a9f3
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2024-10-09 19:24:03 +0500

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,7 +6,7 @@ import 'react-quill/dist/quill.snow.css';
 import { app } from '../Firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import "react-circular-progressbar/dist/styles.css";
-
+import { useNavigate } from 'react-router-dom';
 
 
 function CreatePost() {
@@ -14,6 +14,9 @@ function CreatePost() {
   const [file, setfile] = useState(null);
   const [imageUploadProgress, setimageUploadProgress] = useState(null);
   const [imageUploadError, setimageUploadError] = useState(null);
+  const [publishError, setpublishError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleUploadImage = async () => {
     try {
@@ -53,31 +56,40 @@ function CreatePost() {
     }
   };
 
-  // const handleChange = (e) => {
-  //   setform({ ...form, [e.target.id]: e.target.value })
-  // };
 
-  // const handleCategory = (e) => {
-  //   setform({ ...form, category: e.target.value })
-  // };
+  const handleSubmit =async (e) => {
+    e.preventDefault();
 
-  // const handleContent = (value) => {
-  //   setform({...form, content :value})
-  // };
+    try {
+      const res =await fetch('/api/post/create',{
+        method : 'POST',
+        headers : {'Content-Type' : 'application/json'},
+        body : JSON.stringify(formData)
+      });
 
-  // const handleSubmit =async () => {
-  //   try {
+      const data =await res.json();
 
-  //   } catch (error) {
+      if (!res.ok) {
+        setpublishError(data.message);
+        return
+      };
 
-  //   }
-  // };
+      if (res.ok) {
+        setpublishError(null);
+        navigate(`/post/${data.slug}`);
+      };
+
+    } catch (error) {
+      setpublishError('Something went wrong');
+    }
+  };
+
 
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>CreatePost</h1>
 
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
             type='text'
@@ -85,9 +97,9 @@ function CreatePost() {
             required
             id='title'
             className='flex-1'
-          // onChange={(e) => handleChange(e)}
+            onChange={(e) => setformData({ ...formData, title: e.target.value })}
           />
-          <Select >
+          <Select onChange={(e) => setformData({...formData, category : e.target.value})}>
             <option value={'uncategorized'} >Select a category</option>
             <option value={'javascript'} >JavaScript</option>
             <option value={'react js'} >React.js</option>
@@ -126,10 +138,18 @@ function CreatePost() {
           <img src={formData.image} className='w-full h-72 object-contain' />
         )}
 
-        <ReactQuill theme='snow' placeholder='Write Something...' className='h-72 mb-12' required />
+        <ReactQuill theme='snow' placeholder='Write Something...' className='h-72 mb-12' required 
+          onChange={(value) => setformData({...formData, content : value})}
+        />
         <Button type='submit' gradientDuoTone={'purpleToPink'}>
           Publish
         </Button>
+
+        {publishError && (
+          <Alert color={'failure'}>
+            {publishError}
+          </Alert>
+        )}
       </form>
     </div>
   )
