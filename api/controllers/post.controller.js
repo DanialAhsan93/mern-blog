982122867c7fea3d48d7590b5225924692d9a5d0
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2024-10-10 02:39:08 +0500

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -28,4 +28,53 @@ export const create = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-}
+};
+
+export const getposts =async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+
+    const posts =await Post.find({
+      ...(req.query.userId && {userId : req.query.userId}),
+      ...(req.query.category && {category : req.query.category}),
+      ...(req.query.slug && {slug : req.query.slug}),
+      ...(req.query.postId && {_id : req.query.postId}),
+      ...(req.query.searchTerm && {
+        $or : [
+          {title : { $regex : req.query.searchTerm, $options : 'i' }},
+          {content : { $regex : req.query.searchTerm, $options : 'i' }}
+        ]
+      })
+    }).sort({updatedAt : sortDirection}).skip({startIndex}).limit(limit);
+
+    // total number of posts in mongo db
+
+    const totalPosts = await Post.countDocuments(); // countDocuments is a method used to count total no of posts in mongo db;
+
+    //How to calculate total number of posts in mongo db last month
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts =await Post.countDocuments({
+      createdAt : {$gte : oneMonthAgo}
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts
+    })
+
+    
+  } catch (error) {
+    next(error)
+  }
+};
