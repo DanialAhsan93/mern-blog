f132eb4b9955e680a6bb0daf9443d4d706f54b28
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2025-03-24 03:46:15 +0500

    complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
@@ -11,6 +12,7 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import Updatepost from './pages/Updatepost';
+import PostPage from './pages/PostPage';
 
 function App() {
 
@@ -31,6 +33,7 @@ function App() {
           <Route path='/signin' element={<Signin />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/projects' element={<Projects />} />
+          <Route path='/post/:postSlug' element={<PostPage />} />
         </Routes>
         <FooterComp />
       </BrowserRouter>
