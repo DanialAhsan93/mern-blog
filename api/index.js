7007f55bd2bb8edb3ce602da6d642df44ac1352b
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2025-04-05 00:02:24 +0500

    correction of vercel json

--- api/index.js
+++ api/index.js
@@ -34,6 +34,11 @@ app.use('/api/comment',commentRoutes);
 
 
 
+app.listen(3000,() => {
+  console.log('server is running on port 3000!');
+});
+
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'internal sever error';
